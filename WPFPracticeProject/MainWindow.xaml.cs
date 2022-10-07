7a754e7b28f50d7f23a1bfd668b1900130b9f0a9
Helen L <htslin103@gmail.com>
2022-10-06 21:41:42 -0600

    Initial commit

---
+++ WPFPracticeProject/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFPracticeProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string dbConnStr = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+        }
+
+        private void btnAddName_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("added name");
+        }
+
+        private void btnRemoveName_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("deleted name");
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+    }
+}
