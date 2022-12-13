bbef358a234043a339d81513f78eec9b0e35a7b2
Helen L <htslin103@gmail.com>
2022-12-12 20:39:08 -0700

    New Digital Library App commit

---
+++ DigitalLibraryApplication/Controllers/Welcome.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalLibraryApplication.Controllers
+{
+    public class Welcome : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to the digital library";
+            return View();
+        }
+
+        public string Description()
+        {
+            return "This is the app for managing digital books";
+        }
+    }
+}
